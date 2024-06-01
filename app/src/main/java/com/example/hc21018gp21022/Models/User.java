ce4b6aab0ef524ba43825e09061f155fe73cc51f
DIGP1 <diegogarci098@gmail.com>
2024-06-01 10:48:47 -0600

    Feat: Creacion de registro y modelo
    Se a creado el registro y se a implementado la funcion de que ya se agrega en la base de firebase

---
+++ app/src/main/java/com/example/hc21018gp21022/Models/User.java
@@ -0,0 +1,11 @@
+package com.example.hc21018gp21022.Models;
+
+import java.util.List;
+
+public class User {
+    public String username;
+    public String email;
+    public String password;
+    public List<String> idFavorite;
+    public List<String> idPost;
+}
