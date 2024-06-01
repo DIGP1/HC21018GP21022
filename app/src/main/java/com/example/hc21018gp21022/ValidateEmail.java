77be9d2250ffd4e3b34f79eb7e26543a33d1766b
DIGP1 <diegogarci098@gmail.com>
2024-06-01 12:36:37 -0600

    Ya funciona el login

---
+++ app/src/main/java/com/example/hc21018gp21022/ValidateEmail.java
@@ -0,0 +1,19 @@
+package com.example.hc21018gp21022;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateEmail {
+    private static final String EMAIL_PATTERN =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    public static boolean isValidEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
